5769254cd28e0ccbf51ebe44717101407ef41382
Ilhyo Sung <sih8189@gmail.com>
2020-02-12 05:20:08 -0500

    Updates

---
+++ precache-manifest.f60e165e9c04b00a957cb24ef93a2641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5cd11a540ce3ba9602c0395ef811d5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a21595e6d6a22735a7c7",
+    "url": "/monsters-rolodex/static/css/main.7456ec93.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21595e6d6a22735a7c7",
+    "url": "/monsters-rolodex/static/js/main.b12956ff.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
